58bf1a91dc1152c20506d523aaf120da32df19c8
Rodrigo Azevedo <rodrigozcma@gmail.com>
2021-04-02 18:48:01 -0300

    feat: adding a global style guide with instance properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { routes } from './pages/routes';
 import GlobalStyle from './styles/global';
+import FullThemeLight from './styles/themes/themes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Switch>
-        {routes.map((route) => (
-          <Route
-            key={route.name}
-            path={route.path}
-            exact
-            component={route.component}
-          />
-        ))}
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={FullThemeLight}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Switch>
+          {routes.map((route) => (
+            <Route
+              key={route.name}
+              path={route.path}
+              exact
+              component={route.component}
+            />
+          ))}
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
