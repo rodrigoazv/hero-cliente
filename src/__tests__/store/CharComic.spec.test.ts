c64d88ef3598149d57ead12a6874548060f3130e
Rodrigo Azevedo <rodrigozcma@gmail.com>
2021-04-11 18:41:46 -0300

    fix(adding char page from comics and adding tests): adding char page from comics and adding tests

--- src/__tests__/store/CharComic.spec.test.ts
+++ src/__tests__/store/CharComic.spec.test.ts
@@ -1,5 +1,5 @@
-import { sendLoginAction, sendRegisterAction, getUserAction, setAuth, refetchCharComicLikeAction, likeCharComicAction} from '../../store/ducks/user/actions'
-import {UserTypes, UserLogin, User, likeCharComics} from '../../store/ducks/user/types'
+import { getCharsAction, getComicsAction, getCharsIdAction, getComicsIdAction, getSearch } from '../../store/ducks/char-comics/actions'
+import {CharComicsTypes, SearchType} from '../../store/ducks/char-comics/types'
 import {NotifyTypes} from '../../store/ducks/notify/types'
 import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk';
@@ -19,19 +19,104 @@ describe('actions', () => {
   afterEach(() => {
     jest.resetAllMocks();
   });
-  it('should create all actions instances for user login', async () => {
+  it('should create all actions instances for char get', async () => {
 
-    (axios.post as jest.MockedFunction<typeof axios.post>).mockResolvedValueOnce({
-      data: { token: 'jwt-example' }
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce({
+      data: { char: [{}] }
     });
 
     const initialState = {}
     const store = mockStore(initialState)
    
-    const expectedPayload = [  NotifyTypes.SET_LOADING, UserTypes.TOGGLE_LOGIN,NotifyTypes.SET_MESSAGE, NotifyTypes.SET_LOADING ]
-    const user: UserLogin ={ email:"rodrigo@gmail.com", password:'1234qwer'}
+    const expectedPayload = [  NotifyTypes.SET_LOADING, CharComicsTypes.TOGGLE_CHAR, NotifyTypes.SET_LOADING ]
+    const page = 10;
+    return store.dispatch<any>(getCharsAction('', page)).then(() => { 
+        
+        const actions = store.getActions().map(action => action.type)
+        expect(actions).toEqual(expectedPayload)
+    })
+    
+  })
+  it('should create all actions instances for comic get', async () => {
 
-    return store.dispatch<any>(sendLoginAction(user)).then(() => { 
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce({
+      data: { comics: [{}] }
+    });
+
+    const initialState = {}
+    const store = mockStore(initialState)
+   
+    const expectedPayload = [  NotifyTypes.SET_LOADING, CharComicsTypes.TOGGLE_COMICS, NotifyTypes.SET_LOADING ]
+    const page = 10;
+    return store.dispatch<any>(getComicsAction('', page)).then(() => { 
+        
+        const actions = store.getActions().map(action => action.type)
+        expect(actions).toEqual(expectedPayload)
+    })
+    
+  })
+  it('should create all actions instances for char get by id', async () => {
+
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce({
+      data: { char: [{}] }
+    });
+
+    const initialState = {}
+    const store = mockStore(initialState)
+   
+    const expectedPayload = [  NotifyTypes.SET_LOADING,CharComicsTypes.TOGGLE_CHAR_BY_ID, NotifyTypes.SET_LOADING ]
+    return store.dispatch<any>(getCharsIdAction('fakeid')).then(() => { 
+        
+        const actions = store.getActions().map(action => action.type)
+        expect(actions).toEqual(expectedPayload)
+    })
+    
+  })
+  it('should create all actions instances for comic get by id', async () => {
+
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce({
+      data: { comics: [{}] }
+    });
+
+    const initialState = {}
+    const store = mockStore(initialState)
+   
+    const expectedPayload = [  NotifyTypes.SET_LOADING, CharComicsTypes.TOGGLE_COMICS_BY_ID, NotifyTypes.SET_LOADING ]
+    return store.dispatch<any>(getComicsIdAction('fakeid')).then(() => { 
+        
+        const actions = store.getActions().map(action => action.type)
+        expect(actions).toEqual(expectedPayload)
+    })
+    
+  })
+  it('should create all actions instances for search char', async () => {
+
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce({
+      data: { char: [{}] }
+    });
+
+    const initialState = {}
+    const store = mockStore(initialState)
+    const search: SearchType = { searchType: 'Char', searchUrl: '/', valueSearch: ''}
+    const expectedPayload = [  NotifyTypes.SET_LOADING, CharComicsTypes.TOGGLE_CHAR, NotifyTypes.SET_LOADING ]
+    return store.dispatch<any>(getSearch(search)).then(() => { 
+        
+        const actions = store.getActions().map(action => action.type)
+        expect(actions).toEqual(expectedPayload)
+    })
+    
+  })
+  it('should create all actions instances for search comics', async () => {
+
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce({
+      data: { char: [{}] }
+    });
+
+    const initialState = {}
+    const store = mockStore(initialState)
+    const search: SearchType = { searchType: 'Comics', searchUrl: '/', valueSearch: ''}
+    const expectedPayload = [  NotifyTypes.SET_LOADING, CharComicsTypes.TOGGLE_COMICS, NotifyTypes.SET_LOADING ]
+    return store.dispatch<any>(getSearch(search)).then(() => { 
         
         const actions = store.getActions().map(action => action.type)
         expect(actions).toEqual(expectedPayload)
